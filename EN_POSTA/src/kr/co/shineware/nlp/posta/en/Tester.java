e1d4bd16c9f00711344b7dd23dd73032bf69a527
jsshin <jsshin@shineware.co.kr>
2014-10-24 00:02:43 +0900

    enposta Tester commit

---
+++ EN_POSTA/src/kr/co/shineware/nlp/posta/en/Tester.java
@@ -0,0 +1,10 @@
+package kr.co.shineware.nlp.posta.en;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		EnPosta enposta = new EnPosta("D:\\00_Workspace\\git\\POSTA\\POSTA\\model_build");
+		enposta.analyze("you");
+	}
+
+}
